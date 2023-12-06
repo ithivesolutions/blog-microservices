5bc3b1e5ad602a11b331257e2ed7383e7129103d
siva <it.hive@outlook.com>
2023-12-06 17:49:51 +1100

    ingress-nginx-added

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import PostList from './PostList';
 
 const App = () => {
     return <div className='container'>
-        <h1>Create Post</h1>
+        <h1>Create Post !!!</h1>
         <PostCreate />
         <h1>Posts</h1>
         <PostList />
